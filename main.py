f784d3cc0cdc7ea5a7a07f61e41648aa42c35767
justyn temme <justyntemme@gmail.com>
2024-07-03 11:06:45 -0500

    refactoring and adding extract time values back in

--- main.py
+++ main.py
@@ -9,25 +9,14 @@ import requests
 
 logging.basicConfig(level=logging.INFO)
 
+
+# Global Variables
+n = None  # To shorten line lengths
 TL_URL = os.environ.get("TL_URL")
 
 
-def extractTimeValues(json_response):
-    data = json.loads(json_response)
-    time_values = []
-    for item in data:
-        if "time" in item:
-            # Convert the 'time' string to a datetime
-            time_value = isoparse(item["time"]).astimezone(pytz.utc)
-            # Format the datetime object to (hours and minutes in AM/PM format)
-            formatted_time = time_value.strftime("%I:%M %p")
-            time_values.append(formatted_time)
-
-    return time_values
-
-
 def getScans(token: str) -> Tuple[int, str]:
-    scanURL = TL_URL + "/api/v1/scans" if TL_URL is not None else exit()
+    scanURL = TL_URL + "/api/v1/scans" if TL_URL is not None else exit(1)
     headers = {
         "accept": "application/json; charset=UTF-8",
         "content-type": "application/json",
@@ -39,7 +28,7 @@ def getScans(token: str) -> Tuple[int, str]:
 
 
 def generateCwpToken(accessKey: str, accessSecret: str) -> Tuple[int, str]:
-    authURL = f"{TL_URL}/api/v1/authenticate" if TL_URL is not None else exit()
+    authURL = f"{TL_URL}/api/v1/authenticate" if TL_URL is not n else exit(1)
 
     headers = {
         "accept": "application/json; charset=UTF-8",
@@ -62,23 +51,41 @@ def generateCwpToken(accessKey: str, accessSecret: str) -> Tuple[int, str]:
     return response.status_code, ""
 
 
+def check_param(param_name: str) -> str:
+    param_value = os.environ.get(param_name)
+    if param_value is None:
+        logging.error(f"Missing {param_name}")
+        raise ValueError(f"Missing {param_name}")
+    return param_value
+
+
+def extractTimeValues(json_response):
+    data = json.loads(json_response)
+    time_values = []
+    for item in data:
+        if "time" in item:
+            # Convert the 'time' string to a datetime
+            time_value = isoparse(item["time"]).astimezone(pytz.utc)
+            # Format the datetime object to (hours and minutes in AM/PM format)
+            formatted_time = time_value.strftime("%I:%M %p")
+            time_values.append(formatted_time)
+
+    return time_values
+
+
 def main():
-    accessKey = os.environ.get("PC_IDENTITY")
-    accessSecret = os.environ.get("PC_SECRET")
-    if accessKey is None or accessSecret is None:
-        logging.error("Missing PC_IDENTITY or PC_SECRET")
-    elif TL_URL is None:
-        logging.error("Missing TL_URL")
+    P: Tuple[str, str, str] = ("PC_IDENTITY", "PC_SECRET", "TL_URL")
+    accessKey, accessSecret, _ = map(check_param, P)
     responseCode, cwpToken = (
         generateCwpToken(accessKey, accessSecret)
         if accessKey and accessSecret
         else (None, None)
     )
 
-    responseCode, content = getScans(cwpToken) if cwpToken else (exit(9))
+    responseCode, content = getScans(cwpToken) if cwpToken else (exit(1))
     logging.info(responseCode)
     timeValues = extractTimeValues(content)
-    print(timeValues)
+    logging.info(timeValues)
 
 
 if __name__ == "__main__":
